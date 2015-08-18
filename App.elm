d3bb33d9107d4c792bf872226a2c6458dc2587da
eeue56 <noah@astraldynamics.co.uk>
2015-08-18 05:20:36 +0100

    basic color swatches

--- App.elm
+++ App.elm
@@ -4,9 +4,9 @@ import Mouse
 import Window
 
 import Graphics.Element exposing (show)
-import Graphics.Collage exposing (collage, segment, solid, traced, path, toForm)
+import Graphics.Collage exposing (..)
 
-import Color exposing (red)
+import Color exposing (Color, red, blue, yellow)
 
 import Random exposing (generate, initialSeed, int)
 
@@ -15,38 +15,62 @@ mouseMailbox : Signal.Mailbox Action
 mouseMailbox = Signal.mailbox Nothing
 
 type alias Model = {
-    points : List (Float, Float),
-    windowSize : (Float, Float)
+  color : Color,
+  points : List (Float, Float),
+  mouseAt : (Float, Float),
+  windowSize : (Float, Float)
 }
 
-type Action = MouseMove (Float, Float) | WindowResize (Float, Float) | Nothing
+type Action = 
+  MouseClick (Float, Float) | 
+  MouseMove (Float, Float) | 
+  WindowResize (Float, Float) | 
+  Nothing
 
 model : Model
 model = {
+  color = red,
   points = [],
-  windowSize = (500, 500) }
+  mouseAt = (50, 50),
+  windowSize = (3000, 3000) }
 
 drawPoints model = 
-  traced (solid red) 
-  <| path <| model.points
+  traced (solid model.color) 
+    <| path model.points
+
+drawBlotch color = 
+  rect 50 50
+    |> filled color
+
+drawPointer model = 
+  oval 5 5
+    |> filled model.color
+    |> move model.mouseAt
 
 view address model = 
   collage (round <| fst model.windowSize) (round <| snd model.windowSize) [
     drawPoints model,
-    toForm <| show model
+    drawPointer model
   ]
+
+scaledValues x y model =
+  let 
+    winX = fst model.windowSize
+    winY = snd model.windowSize
+    newX = x - (winX / 2)
+    newY = ((winY / 2)) - y
+  in
+    (newX, newY)
 
 update : Action -> Model -> Model 
 update action model =
   case action of 
-    MouseMove (x, y) ->
+    MouseClick (x, y) ->
       let
-        winX = fst model.windowSize
-        winY = snd model.windowSize
-        newX = x - (winX / 2)
-        newY = ((winY / 2)) - y
+        (newX, newY) = scaledValues x y model
       in 
         { model | points <- (newX, newY) :: model.points }
+    MouseMove (x, y) -> { model | mouseAt <- scaledValues x y model }
     WindowResize (x, y) -> { model | windowSize <- (x, y)}
     Nothing -> model
 
@@ -55,9 +79,11 @@ model' =
     update
     model
     <|
-      Signal.merge
-        (Signal.map (\(x, y) -> WindowResize (toFloat x, toFloat y)) Window.dimensions)
-        (Signal.map2 (\isDown (x, y) -> if isDown then MouseMove (toFloat x, toFloat y) else Nothing) Mouse.isDown Mouse.position )
-
+      Signal.mergeMany
+        [ Signal.map (\(x, y) -> WindowResize (toFloat x, toFloat y)) Window.dimensions,
+          (Signal.map2 
+            (\isDown (x, y) -> if isDown then MouseClick (toFloat x, toFloat y) else MouseMove (toFloat x, toFloat y)) 
+            Mouse.isDown Mouse.position )
+           ]
 
 main = Signal.map (view mouseMailbox.address) model'
